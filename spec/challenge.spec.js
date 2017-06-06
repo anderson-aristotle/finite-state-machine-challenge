ab93befb17d24b0db19a6edecca93fa4640c30e6
jordanallain <jallain@gmail.com>
2017-06-06 11:48:22 -0400

    Use 'open' and 'closed' to represent state
    Replace true and false with 'open' and 'closed' to
    represent state in SubwayGate

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -6,21 +6,20 @@ const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 
 const expect = chai.expect
-// const assert = chai.assert
 
 const challenge = require('../lib/challenge')
 
 describe('Finite State Machine', function () {
   const gate = new challenge.SubwayGate()
 
-  it('has an initial state that is false', function () {
-    expect(gate.state).to.equal(false)
+  it('has an initial state that is closed', function () {
+    expect(gate.state).to.equal('closed')
   })
 
   it('has a walkThrough method that transitions state to false', function () {
     gate.walkThrough()
     expect(gate.walkThrough).to.be.function
-    expect(gate.state).to.equal(false)
+    expect(gate.state).to.equal('closed')
   })
 
   it('has an exit method that transitions state to true, and invokes the walkThrough method', function () {
