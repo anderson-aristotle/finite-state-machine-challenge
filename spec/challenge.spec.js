6f60fa8091793fe214e086560a7b402efab46cee
jordanallain <jallain@gmail.com>
2017-06-06 10:19:41 -0400

    Add walkThrough method to SubwayGate
    Add walkThough method to SubwayGate that transitions
    state to false

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -12,7 +12,12 @@ const challenge = require('../lib/challenge')
 describe('Finite State Machine', function () {
   const gate = new challenge.SubwayGate()
 
-  it('has a state that is false', function () {
+  it('has an initial state that is false', function () {
+    expect(gate.state).to.equal(false)
+  })
+
+  it('has a walkThrough method that transitions state to false', function () {
+    gate.walkThrough()
     expect(gate.state).to.equal(false)
   })
 })
