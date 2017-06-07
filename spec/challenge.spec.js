b2ad854e3edef7b8d96b3d31f0aca263c3d2ff14
jordanallain <jallain@gmail.com>
2017-06-07 11:28:47 -0400

    Test presence of methods in first describe block
    In initial describe block, test that each method exists and
    is indeed a function.

--- spec/challenge.spec.js
+++ spec/challenge.spec.js
@@ -16,19 +16,38 @@ describe('SubwayGate', function () {
     expect(gate.state).to.equal('closed')
   })
 
+  it('has a tapCard method', function () {
+    expect(gate.tapCard).to.be.a.function
+  })
+
+  it('has an insertTicket method', function () {
+    expect(gate.insertTicket).to.be.a.function
+  })
+
+  it('has a walkthrough method', function () {
+    expect(gate.walkthrough).to.be.a.function
+  })
+
+  it('has an exit method', function () {
+    expect(gate.exit).to.be.a.function
+  })
+
   describe('tapCard method', function () {
     const charlieCard = {
       monthlyValue: true
     }
+
     it("transitions state to open if the charlieCard's monthlyValue is true", function () {
-      expect(gate.tapCard).to.be.a.function
       gate.tapCard(charlieCard)
       expect(gate.state).to.equal('open')
     })
+
     it('does not transition state to closed if invoked while state is open', function () {
       gate.state = 'open'
       expect(gate.tapCard(charlieCard)).to.equal(true)
+      expect(gate.state).to.equal('open')
     })
+
     it('does not transition state to open if monthlyValue is false', function () {
       gate.state = 'closed'
       charlieCard.monthlyValue = false
@@ -41,14 +60,16 @@ describe('SubwayGate', function () {
       value: 10
     }
     const initialValue = charlieTicket.value
+
     it('transitions state to open if the charlieTicket has a value greater or equal to 2.25', function () {
-      expect(gate.insertTicket).to.be.a.function
       gate.insertTicket(charlieTicket)
       expect(gate.state).to.equal('open')
     })
+
     it('subtracts 2.25 from charlieTicket after successfully transitioning state to open', function () {
       expect(charlieTicket.value).to.equal(initialValue - 2.25)
     })
+
     it('does not subtract 2.25 from charlieTicket if insertTicket is invoked while state is currently open', function () {
       charlieTicket.value = 10
       gate.state = 'open'
@@ -69,7 +90,6 @@ describe('SubwayGate', function () {
 
     // it('has an insertTicket method that transitions state to open if the Charlie Ticket has a value greater or equal to 2.25 & then subtracts 2.25 from the value', function () {
     //   const ticketValue = challenge.charlieTicket.value
-    //   expect(gate.insertTicket).to.be.a.function
     //   if (gate.insertTicket()) {
     //     expect(gate.state).to.equal('open')
     //   } else {
